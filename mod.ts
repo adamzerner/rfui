a83a69d41f9b0aca4d488a6341ca23bd8ca50432
Adam Zerner <adamzerner@protonmail.com>
2023-11-21 17:18:16 -0800

    Add `addOne` function

--- mod.ts
+++ mod.ts
@@ -1,1 +1,12 @@
 export { sayHello } from "./say-hello";
+
+/**
+ * This is a description of the `addOne` function.
+ * 
+ * @example
+ * // returns 4
+ * `addOne(3)`;
+ */
+export const addOne = (n: number) => {
+  return n + 1;
+}

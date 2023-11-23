f6e10cc30a3321878153357d09a6265f3eedd821
Adam Zerner <adamzerner@protonmail.com>
2023-11-23 15:36:04 -0800

    Do most of work for badge

--- components/atoms/badge.tsx
+++ components/atoms/badge.tsx
@@ -1,6 +1,6 @@
 import { ComponentChild, JSX } from "preact";
 
-// TODO: default with different colors; rounded (sm, lg, square, full); with icon
+// TODO: with icon
 
 /** *
  * @function Badge
@@ -10,23 +10,58 @@ import { ComponentChild, JSX } from "preact";
  */
 export const Badge = (
   {
+    type = "neutral",
+    size = "md",
+    rounded = "sm",
     children,
     ...rest
   }: {
-    underline?: "always" | "hover" | "none";
+    type?: "neutral" | "info" | "success" | "warning" | "danger";
+    size?: "sm" | "md" | "lg";
+    rounded?: "square" | "sm" | "lg" | "full";
     children: ComponentChild;
-  } & JSX.HTMLAttributes<HTMLDivElement>,
+  } & Omit<JSX.HTMLAttributes<HTMLDivElement>, "size">,
 ) => {
-  let className = "cursor-pointer";
+  let className = "";
 
-  // className += " " + (() => {
-  //   switch (underline) {
-  //     case "always":
-  //       return "underline";
-  //     case "hover":
-  //       return "hover:underline";
-  //   }
-  // })();
+  className += " " + (() => {
+    switch (type) {
+      case "neutral":
+        return "bg-neutral-50 text-neutral-900";
+      case "info":
+        return "bg-primary-50 text-primary-900";
+      case "success":
+        return "bg-supporting-green-50 text-supporting-green-900";
+      case "warning":
+        return "bg-supporting-yellow-50 text-supporting-yellow-900";
+      case "danger":
+        return "bg-supporting-red-50 text-supporting-red-900";
+    }
+  })();
+
+  className += " " + (() => {
+    switch (size) {
+      case "sm":
+        return "px-1 text-xs";
+      case "md":
+        return "px-2 py-1 text-sm";
+      case "lg":
+        return "px-3 py-2 text-md";
+    }
+  })();
+
+  className += " " + (() => {
+    switch (rounded) {
+      case "square":
+        return "rounded-none";
+      case "sm":
+        return "rounded";
+      case "lg":
+        return "rounded-lg";
+      case "full":
+        return "rounded-full";
+    }
+  })();
 
   if (rest.class) {
     className += ` ${rest.class}`;
--- routes/atoms/badge.tsx
+++ routes/atoms/badge.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/atoms/badge.tsx";
 import { Link } from "@/components/atoms/link.tsx";
 import { Examples } from "@/islands/demo/examples.tsx";
 
@@ -10,11 +11,66 @@ export default () => {
           https://github.com/adamzerner/rfui/blob/master/routes/atoms/badge.tsx
         </Link>
       </div>
-      <Examples title="Underline">
+      <Examples title="Type">
         <div class="flex gap-3">
-          <Link underline="always" href="https://example.com">always</Link>
-          <Link underline="hover" href="https://example.com">hover</Link>
-          <Link underline="none" href="https://example.com">none</Link>
+          <Badge type="neutral">neutral</Badge>
+          <Badge type="info">info</Badge>
+          <Badge type="success">success</Badge>
+          <Badge type="warning">warning</Badge>
+          <Badge type="danger">danger</Badge>
+        </div>
+      </Examples>
+      <Examples title="Size">
+        <div class="flex gap-3">
+          <Badge type="neutral" size="sm">neutral</Badge>
+          <Badge type="info" size="sm">info</Badge>
+          <Badge type="success" size="sm">success</Badge>
+          <Badge type="warning" size="sm">warning</Badge>
+          <Badge type="danger" size="sm">danger</Badge>
+        </div>
+        <div class="flex gap-3">
+          <Badge type="neutral" size="md">neutral</Badge>
+          <Badge type="info" size="md">info</Badge>
+          <Badge type="success" size="md">success</Badge>
+          <Badge type="warning" size="md">warning</Badge>
+          <Badge type="danger" size="md">danger</Badge>
+        </div>
+        <div class="flex gap-3">
+          <Badge type="neutral" size="lg">neutral</Badge>
+          <Badge type="info" size="lg">info</Badge>
+          <Badge type="success" size="lg">success</Badge>
+          <Badge type="warning" size="lg">warning</Badge>
+          <Badge type="danger" size="lg">danger</Badge>
+        </div>
+      </Examples>
+      <Examples title="Rounded">
+        <div class="flex gap-3">
+          <Badge type="neutral" rounded="square">square</Badge>
+          <Badge type="info" rounded="square">square</Badge>
+          <Badge type="success" rounded="square">square</Badge>
+          <Badge type="warning" rounded="square">square</Badge>
+          <Badge type="danger" rounded="square">square</Badge>
+        </div>
+        <div class="flex gap-3">
+          <Badge type="neutral" rounded="sm">sm</Badge>
+          <Badge type="info" rounded="sm">sm</Badge>
+          <Badge type="success" rounded="sm">sm</Badge>
+          <Badge type="warning" rounded="sm">sm</Badge>
+          <Badge type="danger" rounded="sm">sm</Badge>
+        </div>
+        <div class="flex gap-3">
+          <Badge type="neutral" rounded="lg">lg</Badge>
+          <Badge type="info" rounded="lg">lg</Badge>
+          <Badge type="success" rounded="lg">lg</Badge>
+          <Badge type="warning" rounded="lg">lg</Badge>
+          <Badge type="danger" rounded="lg">lg</Badge>
+        </div>
+        <div class="flex gap-3">
+          <Badge type="neutral" rounded="full">full</Badge>
+          <Badge type="info" rounded="full">full</Badge>
+          <Badge type="success" rounded="full">full</Badge>
+          <Badge type="warning" rounded="full">full</Badge>
+          <Badge type="danger" rounded="full">full</Badge>
         </div>
       </Examples>
     </div>

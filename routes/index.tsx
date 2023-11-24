c0818928d4cd077a36541fe68f9ef5921e54fd69
Adam Zerner <adamzerner@protonmail.com>
2023-11-23 18:40:30 -0800

    Tweak

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,16 +7,13 @@ export default () => {
         <div>Atoms</div>
         <ul class="list-disc list-inside">
           <li>
+            <Link href="/atoms/badge">Badge</Link>
+          </li>
+          <li>
             <Link href="/atoms/button">Button</Link>
           </li>
           <li>
             <Link href="/atoms/link">Link</Link>
-          </li>
-          <li>
-            <Link href="/atoms/new-tab-icon">NewTabIcon</Link>
-          </li>
-          <li>
-            <Link href="/atoms/badge">Badge</Link>
           </li>
         </ul>
       </div>
@@ -28,6 +25,14 @@ export default () => {
       <div>
         <div>Organisms</div>
         <ul>
+        </ul>
+      </div>
+      <div>
+        <div>Icons</div>
+        <ul class="list-disc list-inside">
+          <li>
+            <Link href="/atoms/new-tab-icon">NewTabIcon</Link>
+          </li>
         </ul>
       </div>
     </div>

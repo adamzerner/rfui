2498c74c10965fd0e469b48d67401be144e31e0b
Adam Zerner <adamzerner@protonmail.com>
2023-11-23 22:52:40 -0800

    Finish input

--- components/atoms/input.tsx
+++ components/atoms/input.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from "preact";
 
-// TODO: disabled, readonly, helper text, error text, invalid, required, focus state
+// TODO: type
 /** *
  * @function Input
  * *
@@ -11,13 +11,27 @@ export const Input = (
   {
     size = "md",
     rounded = "sm",
+    invalid = false,
     ...rest
   }: {
     size?: "sm" | "md" | "lg";
     rounded?: "square" | "sm" | "lg" | "full";
+    invalid?: boolean;
   } & Omit<JSX.HTMLAttributes<HTMLInputElement>, "size">,
 ) => {
-  let className = "border-1 border-neutral-300 px-2 py-1";
+  let className =
+    "border-1 border-neutral-300 px-2 py-1 focus:outline-none focus:border-neutral-700 focus:shadow-sm";
+
+  className += " " + (() => {
+    switch (size) {
+      case "sm":
+        return "px-2 text-sm";
+      case "md":
+        return "px-2 py-1";
+      case "lg":
+        return "px-3 py-2 text-lg";
+    }
+  })();
 
   className += " " + (() => {
     switch (rounded) {
@@ -31,6 +45,19 @@ export const Input = (
         return "rounded-full px-3";
     }
   })();
+
+  if (rest.disabled) {
+    className += " cursor-not-allowed bg-neutral-50";
+  }
+
+  if (rest.readOnly || rest.readonly) {
+    className += " cursor-not-allowed";
+  }
+
+  if (invalid) {
+    className +=
+      " border-supporting-red-300 bg-supporting-red-50 text-supporting-red-900 focus:border-supporting-red-700";
+  }
 
   if (rest.class) {
     className += ` ${rest.class}`;
--- routes/atoms/input.tsx
+++ routes/atoms/input.tsx
@@ -11,12 +11,42 @@ export default () => {
           https://github.com/adamzerner/rfui/blob/master/routes/atoms/input.tsx
         </Link>
       </div>
+      <Examples title="Size">
+        <div class="flex flex-col gap-5">
+          <Input size="sm" />
+          <Input size="md" />
+          <Input size="lg" />
+        </div>
+      </Examples>
       <Examples title="Rounded">
         <div class="flex flex-col gap-5">
           <Input rounded="square" />
           <Input rounded="sm" />
           <Input rounded="lg" />
           <Input rounded="full" />
+        </div>
+      </Examples>
+      <Examples title="Disabled">
+        <Input disabled />
+      </Examples>
+      <Examples title="Readonly">
+        <Input readonly value="example" />
+      </Examples>
+      <Examples title="Invalid">
+        <Input invalid value="example" />
+      </Examples>
+      <Examples title="Type">
+        <div class="flex flex-col gap-5">
+          <Input type="text" placeholder="text" />
+          <Input type="email" placeholder="email" />
+          <Input type="number" />
+          <Input type="password" value="foobar" />
+          <Input type="date" />
+          <Input type="datetime-local" />
+          <Input type="time" />
+          <Input type="file" />
+          <Input type="range" />
+          <Input type="color" />
         </div>
       </Examples>
     </div>

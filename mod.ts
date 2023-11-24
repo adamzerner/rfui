97d550a2a6d9ee263c073f937eed85ebba71a0a5
Adam Zerner <adamzerner@protonmail.com>
2023-11-23 22:54:17 -0800

    Export `Input` from `mod.ts`

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 export { Badge } from "./components/atoms/badge.tsx";
 export { Button } from "./components/atoms/button.tsx";
+export { Input } from "./components/atoms/input.tsx";
 export { Link } from "./components/atoms/link.tsx";
 
 export { NewTabIcon } from "./components/icons/new-tab-icon.tsx";

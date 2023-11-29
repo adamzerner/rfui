0f1a2964c2b414fe3b95d54ebf98f5dfccfd6db7
Adam Zerner <adamzerner@protonmail.com>
2023-11-28 20:51:35 -0800

    Tweak

--- islands/molecules/password-input.tsx
+++ islands/molecules/password-input.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/components/atoms/input.tsx";
+import { JSX } from "preact";
 import { useState } from "preact/hooks";
 
 /** *
@@ -12,13 +13,18 @@ export const PasswordInput = (
     size = "md",
     rounded = "sm",
     invalid = false,
+    defaultVisibility = "hidden",
+    ...rest
   }: {
     size?: "sm" | "md" | "lg";
     rounded?: "square" | "sm" | "lg" | "full";
     invalid?: boolean;
-  },
+    defaultVisibility?: "hidden" | "shown";
+  } & Omit<JSX.HTMLAttributes<HTMLDivElement>, "size">,
 ) => {
-  const [shouldShow, setShouldShow] = useState(false);
+  const [shouldShow, setShouldShow] = useState(
+    defaultVisibility === "hidden" ? false : true,
+  );
   const toggleShouldShow = () => {
     setShouldShow((v) => !v);
   };
@@ -47,14 +53,18 @@ export const PasswordInput = (
   })();
 
   return (
-    <div class="flex gap-1 items-stretch">
+    <div class="flex gap-1 items-stretch" {...rest}>
       <Input
         type={shouldShow ? "text" : "password"}
         size={size}
         rounded={rounded}
         invalid={invalid}
       />
-      <button onClick={toggleShouldShow} class={buttonClass}>
+      <button
+        onClick={toggleShouldShow}
+        class={buttonClass}
+        style={{ minWidth: 45 }}
+      >
         {shouldShow ? "Hide" : "Show"}
       </button>
     </div>

dbcb1a92403d5e4a483c4c8f3346b89c9e1f61b4
Adam Zerner <adamzerner@protonmail.com>
2023-12-04 23:49:05 -0800

    Tweak

--- routes/atoms/highlight.tsx
+++ routes/atoms/highlight.tsx
@@ -20,10 +20,11 @@ export default () => {
           <Highlight>dolor sit amet</Highlight>, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
           enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-          ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          ut aliquip ex ea commodo consequat.{" "}
+          <Highlight>Duis aute irure</Highlight>{" "}
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+          in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </Stack>
     </Stack>

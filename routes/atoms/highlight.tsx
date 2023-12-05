fba6f8615aec93c69abf46f37d1ed53ddf532086
Adam Zerner <adamzerner@protonmail.com>
2023-12-04 23:48:34 -0800

    Add `Highlight` examples

--- routes/atoms/highlight.tsx
+++ routes/atoms/highlight.tsx
@@ -11,11 +11,21 @@ export default () => {
           https://github.com/adamzerner/rfui/blob/master/routes/atoms/highlight.tsx
         </Link>
       </div>
-      <div>
-        <div>
+      <Stack class="gap-5">
+        <h1 class="text-3xl">
           Lorem ipsum <Highlight>dolor</Highlight> sit amet, consectetur.
+        </h1>
+        <div>
+          Lorem ipsum{" "}
+          <Highlight>dolor sit amet</Highlight>, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </div>
-      </div>
+      </Stack>
     </Stack>
   );
 };

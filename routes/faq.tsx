e5c0e9eac285e62d7cae312a62b9d4c164b4b329
Adam Zerner <adamzerner@protonmail.com>
2023-12-10 21:13:35 -0800

    Update faq.tsx

--- routes/faq.tsx
+++ routes/faq.tsx
@@ -1,3 +1,12 @@
+import { Stack } from "@/components/helpers/stack.tsx";
+
 export default () => {
-  return <div>Under construction</div>;
+  return (
+    <div class="max-w-prose text-lg">
+      <h1 class="text-4xl text-neutral-700 mt-9 mb-7">FAQ</h1>
+      <Stack class="gap-4 leading-relaxed">
+        Under construction.
+      </Stack>
+    </div>
+  );
 };

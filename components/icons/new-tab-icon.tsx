020a5a4197113faf4a655032394bf583afe68740
Adam Zerner <adamzerner@protonmail.com>
2023-12-15 20:30:21 -0800

    Tweak

--- components/icons/new-tab-icon.tsx
+++ components/icons/new-tab-icon.tsx
@@ -3,19 +3,17 @@ export const NewTabIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
       viewBox="0 0 24 24"
-      class="w-4 inline relative bottom-[1px] underline"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 inline relative bottom-[1px] left-1 underline"
     >
       <path
-        style={{ fill: "#0d2b3e" }}
-        d="M12 8a1 1 0 0 1-1 1H5v10h10v-6a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9c0-1.1.9-2 2-2h6a1 1 0 0 1 1 1z"
-      >
-      </path>
-      <path
-        style={{ fill: "#0d2b3e" }}
-        d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41z"
-      >
-      </path>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+      />
     </svg>
   );
 };

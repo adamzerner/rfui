e72ee43c5cb4c8a030669b19e47dd29954a83fc9
Adam Zerner <adamzerner@protonmail.com>
2023-12-19 22:27:17 -0800

    Update about.tsx

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,3 +1,4 @@
+import { Blockquote } from "@/components/atoms/blockquote.tsx";
 import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { H1 } from "@/components/atoms/h1.tsx";
 import { H2 } from "@/components/atoms/h2.tsx";
@@ -179,6 +180,102 @@ const SubmitButton = () => {
             score of 100 for accessibility.
           </li>
         </UL>
+      </Text>
+
+      <H2>The Documentation System</H2>
+      <Text>
+        <p>
+          The docs here aim to follow{" "}
+          <Link href="https://documentation.divio.com/">
+            The Documentation System
+          </Link>.
+        </p>
+        <Blockquote>
+          <p>
+            There is a secret that needs to be understood in order to write good
+            software documentation: there isn’t one thing called documentation,
+            there are four.
+          </p>
+          <p>
+            They are: tutorials, how-to guides, technical reference and
+            explanation. They represent four different purposes or functions,
+            and require four different approaches to their creation.
+            Understanding the implications of this will help improve most
+            documentation - often immensely.
+          </p>
+        </Blockquote>
+        <p class="mt-5">
+          I'd really recommend reading through{" "}
+          <Link href="https://documentation.divio.com/">their website</Link>,
+          but in the meantime, this diagram kinda sums it up:
+        </p>
+        <img
+          src="https://documentation.divio.com/_images/overview.png"
+          alt="Diagram of The Documentation System"
+        />
+      </Text>
+
+      <H2>Refactoring UI</H2>
+      <Text>
+        <p>
+          Last but not least, I want to give a shout out to one of the best
+          books I've ever read and the inspiration for this project:{" "}
+          <Link href="https://www.refactoringui.com/">
+            Refactoring UI
+          </Link>.
+        </p>
+        <p>
+          I've been a web developer for the past 10 years. A web developer with
+          a strong interest in design (and startups). I've always wanted to be
+          － not necessarily an expert － but I've always wanted to be{" "}
+          <em>capable</em>{" "}
+          as a designer. I've always wanted to be able to build things that look
+          pretty good. I'm ok with them not winning any design awards, but I do
+          want them to delight users.
+        </p>
+        <p>
+          I definitely don't want them to look like some Bootstrap template.
+          That just screams "I'm an amateur who doesn't really know what they're
+          doing and you shouldn't take my project seriously". But unfortunately,
+          I feel like that's where I've been my whole career. Check out{" "}
+          <Link href="https://premiumpokertools.com/">
+            Premium Poker Tools
+          </Link>{" "}
+          and judge for yourself.
+        </p>
+        <p>
+          And it's certainly not for a lack of trying. I've read so many books.
+          {" "}
+          <Link href="https://www.goodreads.com/book/show/840.The_Design_of_Everyday_Things">
+            The Design of Everyday Things
+          </Link>.{" "}
+          <Link href="https://www.goodreads.com/book/show/3296910-web-form-design">
+            Web Form Design: Filling in the Blanks
+          </Link>.{" "}
+          <Link href="https://www.goodreads.com/book/show/17744.The_Visual_Display_of_Quantitative_Information_2nd_Ed_">
+            The Visual Display of Quantitative Information
+          </Link>.{" "}
+          <Link href="https://www.goodreads.com/book/show/18197267-don-t-make-me-think-revisited">
+            Don't Make Me Think
+          </Link>.{" "}
+          <Link href="https://www.goodreads.com/book/show/6658783-rocket-surgery-made-easy">
+            Rocket Surgery Made Easy
+          </Link>.{" "}
+          <Link href="https://practicaltypography.com/">
+            Practical Typography
+          </Link>. But I've spent much more of my time reading random block
+          posts from Hacker News. Also tons of browsing{" "}
+          <Link href="https://www.nngroup.com/">NNGroup</Link> and{" "}
+          <Link href="https://ux.stackexchange.com/">UX Stack Exchange</Link>.
+        </p>
+        <p>
+          Anyway. I promise no one is paying me to say this, but after reading
+          Refactoring UI, I feel like it may have gotten me over the hump. So
+          thank you to the authors. And to the people reading this, a) go read
+          Refactoring UI and b) I figure maybe it's helpful context to explain
+          that a lot of what I learned in that book has inspired this component
+          library.
+        </p>
       </Text>
     </div>
   );

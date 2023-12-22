373ab76e9e9098889dd96be19a8ff0e9a6570a49
Adam Zerner <adamzerner@protonmail.com>
2023-12-21 23:26:37 -0800

    Add docs for `InlineCode`

--- routes/atoms/inline-code.tsx
+++ routes/atoms/inline-code.tsx
@@ -1,22 +1,39 @@
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { InlineCode } from "@/components/atoms/inline-code.tsx";
-import { Link } from "@/components/atoms/link.tsx";
-import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/inline-code.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/inline-code.tsx
-        </Link>
+        When I wrote <InlineCode>sayHello("John")</InlineCode> it didn't work.
       </div>
-      <Examples title="Default">
-        <div>
-          When I wrote <InlineCode>sayHello("John")</InlineCode> it didn't work.
-        </div>
-      </Examples>
-    </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<div>
+  When I wrote <InlineCode>sayHello("John")</InlineCode> it didn't work.
+</div>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="InlineCode"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/inline-code.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

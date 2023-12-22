ffc501e228471c22e15c6f04fa4393c92f631ed2
Adam Zerner <adamzerner@protonmail.com>
2023-12-22 02:19:53 -0800

    Add docs for `Select`

--- routes/atoms/select.tsx
+++ routes/atoms/select.tsx
@@ -1,74 +1,224 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
 import { Select } from "@/components/atoms/select.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => (
+      <Select>
+        <option value="foo">foo</option>
+        <option value="bar">bar</option>
+        <option value="baz">baz</option>
+      </Select>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Select>
+  <option value="foo">foo</option>
+  <option value="bar">bar</option>
+  <option value="baz">baz</option>
+</Select>`}
+      />
+    ),
+  }, {
+    title: "Size",
+    description: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/select.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/select.tsx
-        </Link>
+        Set <InlineCode>size</InlineCode> to either{" "}
+        <InlineCode>"sm"</InlineCode>, <InlineCode>"md"</InlineCode> or{" "}
+        <InlineCode>"lg"</InlineCode>. Defaults to{" "}
+        <InlineCode>"md"</InlineCode>.
       </div>
-      <Examples title="Size">
-        <Stack class="gap-5">
-          <Select size="sm">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-          <Select size="md">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-          <Select size="lg">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-        </Stack>
-      </Examples>
-      <Examples title="Rounded">
-        <Stack class="gap-5">
-          <Select rounded="square">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-          <Select rounded="sm">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-          <Select rounded="lg">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-          <Select rounded="full">
-            <option value="foo">foo</option>
-            <option value="bar">bar</option>
-            <option value="baz">baz</option>
-          </Select>
-        </Stack>
-      </Examples>
-      <Examples title="Disabled">
-        <Select disabled>
+    ),
+    example: () => (
+      <Stack class="gap-5 w-fit">
+        <Select size="sm">
           <option value="foo">foo</option>
           <option value="bar">bar</option>
           <option value="baz">baz</option>
         </Select>
-      </Examples>
-      <Examples title="Invalid">
-        <Select invalid>
+        <Select size="md">
           <option value="foo">foo</option>
           <option value="bar">bar</option>
           <option value="baz">baz</option>
         </Select>
-      </Examples>
-    </Stack>
+        <Select size="lg">
+          <option value="foo">foo</option>
+          <option value="bar">bar</option>
+          <option value="baz">baz</option>
+        </Select>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5 w-fit">
+  <Select size="sm">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+  <Select size="md">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+  <Select size="lg">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Rounded",
+    description: (
+      <div>
+        Set <InlineCode>rounded</InlineCode> to either{" "}
+        <InlineCode>"square"</InlineCode>, <InlineCode>"sm"</InlineCode>, or
+        {" "}
+        <InlineCode>"lg"</InlineCode>. Defaults to{" "}
+        <InlineCode>"sm"</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Stack class="gap-5 w-fit">
+        <Select rounded="square">
+          <option value="foo">foo</option>
+          <option value="bar">bar</option>
+          <option value="baz">baz</option>
+        </Select>
+        <Select rounded="sm">
+          <option value="foo">foo</option>
+          <option value="bar">bar</option>
+          <option value="baz">baz</option>
+        </Select>
+        <Select rounded="lg">
+          <option value="foo">foo</option>
+          <option value="bar">bar</option>
+          <option value="baz">baz</option>
+        </Select>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5 w-fit">
+  <Select rounded="square">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+  <Select rounded="sm">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+  <Select rounded="lg">
+    <option value="foo">foo</option>
+    <option value="bar">bar</option>
+    <option value="baz">baz</option>
+  </Select>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Disabled",
+    description: (
+      <div>
+        Set <InlineCode>disabled</InlineCode> to either{" "}
+        <InlineCode>true</InlineCode> or{" "}
+        <InlineCode>false</InlineCode>. Defaults to{" "}
+        <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Select disabled>
+        <option value="foo">foo</option>
+        <option value="bar">bar</option>
+        <option value="baz">baz</option>
+      </Select>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Select disabled>
+  <option value="foo">foo</option>
+  <option value="bar">bar</option>
+  <option value="baz">baz</option>
+</Select>`}
+      />
+    ),
+  }, {
+    title: "Invalid",
+    description: (
+      <div>
+        Set <InlineCode>invalid</InlineCode> to either{" "}
+        <InlineCode>true</InlineCode> or{" "}
+        <InlineCode>false</InlineCode>. Defaults to{" "}
+        <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Select invalid>
+        <option value="foo">foo</option>
+        <option value="bar">bar</option>
+        <option value="baz">baz</option>
+      </Select>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Select invalid>
+  <option value="foo">foo</option>
+  <option value="bar">bar</option>
+  <option value="baz">baz</option>
+</Select>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "size",
+    required: false,
+    type: '"sm" | "md" | "lg"',
+    default: '"md"',
+    notes: null,
+  }, {
+    name: "rounded",
+    required: false,
+    type: '"square" | "sm" | "lg" | "full"',
+    default: '"sm"',
+    notes: null,
+  }, {
+    name: "disabled",
+    required: false,
+    type: "boolean",
+    default: "false",
+    notes: null,
+  }, {
+    name: "invalid",
+    required: false,
+    type: "boolean",
+    default: "false",
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="Select"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/select.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

592e49c69f8afc3f9949a8a871593ec22460a8e3
Adam Zerner <adamzerner@protonmail.com>
2023-12-21 18:53:24 -0800

    Button WIP

--- islands/demo/component-docs-page.tsx
+++ islands/demo/component-docs-page.tsx
@@ -21,6 +21,7 @@ type ComponentDocsPageType = {
   }[];
   props: {
     name: string;
+    required: boolean;
     type: string;
     default: string | null;
     notes: JSX.Element | string | null;
@@ -142,17 +143,25 @@ const Props = ({ props }: { props: ComponentDocsPageType["props"] }) => {
       <div class="grid grid-cols-1 sm:grid-cols-3 gap-y-5 sm:gap-y-10 justify-items-start place-items-start">
         {props.map((prop) => (
           <>
-            <Link
-              href={`#${prop.name.toLowerCase()}`}
-              underline="hover"
-              class="text-2xl first-of-type:mt-0 mt-8 sm:mt-0"
-            >
-              <InlineCode>
-                {prop.name}
-              </InlineCode>
-            </Link>
+            <div>
+              <Link
+                href={`#${prop.name.toLowerCase()}`}
+                underline="hover"
+                class="text-2xl first-of-type:mt-0 mt-8 sm:mt-0"
+              >
+                <InlineCode>
+                  {prop.name}
+                </InlineCode>
+              </Link>
+              {prop.required &&
+                <sup class="text-supporting-red-500 text-2xl ml-1">*</sup>}
+            </div>
             <Stack class="gap-5 col-span-2 text-sm">
-              <div>
+              <div class="self-start">
+                <div class="mb-3">Required</div>
+                <CodeBlock language="ts" code={prop.required.toString()} />
+              </div>
+              <div class="self-start">
                 <div class="mb-3">Type</div>
                 <CodeBlock language="ts" code={prop.type} />
               </div>
--- routes/atoms/button.tsx
+++ routes/atoms/button.tsx
@@ -1,133 +1,209 @@
 import { Button } from "@/components/atoms/button.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { Link } from "@/components/atoms/link.tsx";
-import { Flex } from "@/components/helpers/flex.tsx";
-import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => <Button>Example</Button>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Button>Example</Button>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "variant",
+    required: false,
+    type:
+      '"primary" | "secondary" | "tertiary" | "danger-primary" | "danger-secondary" | "danger-tertiary"',
+    default: '"secondary"',
+    notes: null,
+  }, {
+    name: "size",
+    required: false,
+    type: '"sm" | "md" | "lg" | "block"',
+    default: '"md"',
+    notes: null,
+  }, {
+    name: "rounded",
+    required: false,
+    type: '"square" | "sm" | "lg" | "full"',
+    default: '"sm"',
+    notes: null,
+  }, {
+    name: "isLoading",
+    required: false,
+    type: "boolean",
+    default: "false",
+    notes: null,
+  }, {
+    name: "loadingContent",
+    required: false,
+    type: "string | JSX.Element",
+    default: null,
+    notes: null,
+  }, {
+    name: "icon",
+    required: false,
+    type: "JSX.Element",
+    default: null,
+    notes: null,
+  }, {
+    name: "_rightIcon",
+    required: false,
+    type: "JSX.Element",
+    default: null,
+    notes: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/button.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/button.tsx
-        </Link>
+        It is{" "}
+        <Link href="https://ux.stackexchange.com/q/56023/39046">
+          generally preferred
+        </Link>{" "}
+        to place icons to the left of the content rather than the right.
+        However, if you still want to, you can use this property to place the
+        icon to the right.
       </div>
-      <Examples title="Variant">
-        <Flex class="gap-3">
-          <Button variant="primary">
-            Submit
-          </Button>
-          <Button variant="secondary">Submit</Button>
-          <Button variant="tertiary">Submit</Button>
-        </Flex>
-      </Examples>
-      <Examples title="Danger variants">
-        <Flex class="gap-3">
-          <Button variant="danger-primary">
-            Submit
-          </Button>
-          <Button variant="danger-secondary">
-            Submit
-          </Button>
-          <Button variant="danger-tertiary">
-            Submit
-          </Button>
-        </Flex>
-      </Examples>
-      <Examples title="Size">
-        <Flex class="gap-3">
-          <Button variant="primary" size="sm">
-            Submit
-          </Button>
-          <Button variant="secondary" size="sm">Submit</Button>
-          <Button variant="tertiary" size="sm">Submit</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" size="md">
-            Submit
-          </Button>
-          <Button variant="secondary" size="md">Submit</Button>
-          <Button variant="tertiary" size="md">Submit</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" size="lg">
-            Submit
-          </Button>
-          <Button variant="secondary" size="lg">Submit</Button>
-          <Button variant="tertiary" size="lg">Submit</Button>
-        </Flex>
-        <Stack class="gap-3 w-full">
-          <Button variant="primary" size="block">Submit</Button>
-          <Button variant="secondary" size="block">Submit</Button>
-        </Stack>
-      </Examples>
-      <Examples title="Rounded">
-        <Flex class="gap-3">
-          <Button variant="primary" rounded="square">
-            Submit
-          </Button>
-          <Button variant="secondary" rounded="square">Submit</Button>
-          <Button variant="tertiary" rounded="square">Submit</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" rounded="sm">
-            Submit
-          </Button>
-          <Button variant="secondary" rounded="sm">Submit</Button>
-          <Button variant="tertiary" rounded="sm">Submit</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" rounded="lg">
-            Submit
-          </Button>
-          <Button variant="secondary" rounded="lg">Submit</Button>
-          <Button variant="tertiary" rounded="lg">Submit</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" rounded="full">
-            Submit
-          </Button>
-          <Button variant="secondary" rounded="full">Submit</Button>
-          <Button variant="tertiary" rounded="full">Submit</Button>
-        </Flex>
-      </Examples>
-      <Examples title="Icon">
-        <Flex class="gap-3">
-          <Button variant="primary" icon={<span>←</span>}>
-            Previous
-          </Button>
-          <Button variant="secondary" icon={<span>←</span>}>Previous</Button>
-        </Flex>
-        <Flex class="gap-3">
-          <Button variant="primary" _rightIcon={<span>→</span>}>
-            Next
-          </Button>
-          <Button variant="secondary" _rightIcon={<span>→</span>}>Next</Button>
-        </Flex>
-      </Examples>
-      <Examples title="Disabled">
-        <Flex class="gap-3">
-          <Button disabled={true} variant="primary">
-            Submit
-          </Button>
-          <Button disabled={true} variant="secondary">Submit</Button>
-          <Button disabled={true} variant="tertiary">Submit</Button>
-        </Flex>
-      </Examples>
-      <Examples title="Loading">
-        <Flex class="gap-3">
-          <Button variant="primary" isLoading loadingContent="Submitting...">
-            Submit
-          </Button>
-          <Button variant="secondary" isLoading loadingContent="Submitting...">
-            Submit
-          </Button>
-          <Button variant="tertiary" isLoading loadingContent="Submitting...">
-            Submit
-          </Button>
-        </Flex>
-      </Examples>
-    </Stack>
+    ),
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="Button"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/button.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };
+
+// export default () => {
+//   return (
+//     <Stack class="gap-8">
+//       <Examples title="Variant">
+//         <Flex class="gap-3">
+//           <Button variant="primary">
+//             Submit
+//           </Button>
+//           <Button variant="secondary">Submit</Button>
+//           <Button variant="tertiary">Submit</Button>
+//         </Flex>
+//       </Examples>
+//       <Examples title="Danger variants">
+//         <Flex class="gap-3">
+//           <Button variant="danger-primary">
+//             Submit
+//           </Button>
+//           <Button variant="danger-secondary">
+//             Submit
+//           </Button>
+//           <Button variant="danger-tertiary">
+//             Submit
+//           </Button>
+//         </Flex>
+//       </Examples>
+//       <Examples title="Size">
+//         <Flex class="gap-3">
+//           <Button variant="primary" size="sm">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" size="sm">Submit</Button>
+//           <Button variant="tertiary" size="sm">Submit</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" size="md">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" size="md">Submit</Button>
+//           <Button variant="tertiary" size="md">Submit</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" size="lg">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" size="lg">Submit</Button>
+//           <Button variant="tertiary" size="lg">Submit</Button>
+//         </Flex>
+//         <Stack class="gap-3 w-full">
+//           <Button variant="primary" size="block">Submit</Button>
+//           <Button variant="secondary" size="block">Submit</Button>
+//         </Stack>
+//       </Examples>
+//       <Examples title="Rounded">
+//         <Flex class="gap-3">
+//           <Button variant="primary" rounded="square">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" rounded="square">Submit</Button>
+//           <Button variant="tertiary" rounded="square">Submit</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" rounded="sm">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" rounded="sm">Submit</Button>
+//           <Button variant="tertiary" rounded="sm">Submit</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" rounded="lg">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" rounded="lg">Submit</Button>
+//           <Button variant="tertiary" rounded="lg">Submit</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" rounded="full">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" rounded="full">Submit</Button>
+//           <Button variant="tertiary" rounded="full">Submit</Button>
+//         </Flex>
+//       </Examples>
+//       <Examples title="Icon">
+//         <Flex class="gap-3">
+//           <Button variant="primary" icon={<span>←</span>}>
+//             Previous
+//           </Button>
+//           <Button variant="secondary" icon={<span>←</span>}>Previous</Button>
+//         </Flex>
+//         <Flex class="gap-3">
+//           <Button variant="primary" _rightIcon={<span>→</span>}>
+//             Next
+//           </Button>
+//           <Button variant="secondary" _rightIcon={<span>→</span>}>Next</Button>
+//         </Flex>
+//       </Examples>
+//       <Examples title="Disabled">
+//         <Flex class="gap-3">
+//           <Button disabled={true} variant="primary">
+//             Submit
+//           </Button>
+//           <Button disabled={true} variant="secondary">Submit</Button>
+//           <Button disabled={true} variant="tertiary">Submit</Button>
+//         </Flex>
+//       </Examples>
+//       <Examples title="Loading">
+//         <Flex class="gap-3">
+//           <Button variant="primary" isLoading loadingContent="Submitting...">
+//             Submit
+//           </Button>
+//           <Button variant="secondary" isLoading loadingContent="Submitting...">
+//             Submit
+//           </Button>
+//           <Button variant="tertiary" isLoading loadingContent="Submitting...">
+//             Submit
+//           </Button>
+//         </Flex>
+//       </Examples>
+//     </Stack>
+//   );
+// };

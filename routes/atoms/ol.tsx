004e87cc43bbc82c26d1e477c2727c1d9ebbd85e
Adam Zerner <adamzerner@protonmail.com>
2023-12-22 01:45:45 -0800

    Add docs for `OL`

--- routes/atoms/ol.tsx
+++ routes/atoms/ol.tsx
@@ -1,31 +1,69 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { OL } from "@/components/atoms/ol.tsx";
-import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
+  const sections = [{
+    title: "Basic",
+    example: () => (
+      <OL>
+        <li>First</li>
+        <li>Second</li>
+        <li>Third</li>
+      </OL>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<OL>
+  <li>First</li>
+  <li>Second</li>
+  <li>Third</li>
+</OL>`}
+      />
+    ),
+  }, {
+    title: "Outside",
+    example: () => (
+      <OL bulletLocation="outside">
+        <li>First</li>
+        <li>Second</li>
+        <li>Third</li>
+      </OL>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<OL bulletLocation="outside">
+  <li>First</li>
+  <li>Second</li>
+  <li>Third</li>
+</OL>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "bulletLocation",
+    required: false,
+    type: "'inside' | 'outside'",
+    default: "'inside'",
+    notes: null,
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
   return (
-    <Stack class="gap-8">
-      <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/ol.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/ol.tsx
-        </Link>
-      </div>
-      <Examples title="Default">
-        <OL>
-          <li>First</li>
-          <li>Second</li>
-          <li>Third</li>
-        </OL>
-      </Examples>
-      <Examples title="Outside">
-        <OL bulletLocation="outside">
-          <li>First</li>
-          <li>Two</li>
-          <li>Three</li>
-        </OL>
-      </Examples>
-    </Stack>
+    <ComponentDocsPage
+      componentName="OL"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/ol.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

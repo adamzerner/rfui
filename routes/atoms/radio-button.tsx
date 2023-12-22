aee4c8ab571deea47783cec2761cedd64c2c4c77
Adam Zerner <adamzerner@protonmail.com>
2023-12-21 22:52:26 -0800

    Add docs for `RadioButton`

--- routes/atoms/radio-button.tsx
+++ routes/atoms/radio-button.tsx
@@ -1,30 +1,76 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { RadioButton } from "@/components/atoms/radio-button.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
+  const sections = [{
+    title: "Basic",
+    example: () => <RadioButton />,
+    exampleCode: () => (
+      <CodeBlock class="mt-4" language="tsx" code={`<RadioButton />`} />
+    ),
+  }, {
+    title: "Size",
+    example: () => (
+      <Stack class="gap-5">
+        <RadioButton size="sm" />
+        <RadioButton size="md" />
+        <RadioButton size="lg" />
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <RadioButton size="sm" />
+  <RadioButton size="md" />
+  <RadioButton size="lg" />
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Disabled",
+    example: () => <RadioButton disabled />,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<RadioButton disabled />`}
+      />
+    ),
+  }, {
+    title: "Invalid",
+    example: () => <RadioButton invalid />,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<RadioButton invalid />`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "size",
+    required: false,
+    type: '"sm" | "md" | "lg"',
+    default: '"md"',
+    notes: null,
+  }, {
+    name: "invalid",
+    required: false,
+    type: '"sm" | "md" | "lg"',
+    default: '"md"',
+    notes: null,
+  }];
+
   return (
-    <Stack class="gap-8">
-      <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/radio-button.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/radio-button.tsx
-        </Link>
-      </div>
-      <Examples title="Size">
-        <Stack class="gap-5">
-          <RadioButton size="sm" />
-          <RadioButton size="md" />
-          <RadioButton size="lg" />
-        </Stack>
-      </Examples>
-      <Examples title="Disabled">
-        <RadioButton disabled />
-      </Examples>
-      <Examples title="Invalid">
-        <RadioButton invalid />
-      </Examples>
-    </Stack>
+    <ComponentDocsPage
+      componentName="RadioButton"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/radio-button.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

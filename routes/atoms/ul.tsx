6deafd928bea7435a4e310acded7ad26e92756cb
Adam Zerner <adamzerner@protonmail.com>
2023-12-22 02:03:39 -0800

    Add docs for `UL`

--- routes/atoms/ul.tsx
+++ routes/atoms/ul.tsx
@@ -1,143 +1,369 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
 import { UL } from "@/components/atoms/ul.tsx";
 import { Flex } from "@/components/helpers/flex.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => (
+      <UL>
+        <li>First</li>
+        <li>Second</li>
+        <li>Third</li>
+      </UL>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<UL>
+  <li>First</li>
+  <li>Second</li>
+  <li>Third</li>
+</UL>`}
+      />
+    ),
+  }, {
+    // "bullet" | "check" | "arrow" | "triangle" | "none"
+    title: "Bullet type",
+    description: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/ul.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/ul.tsx
-        </Link>
+        Set <InlineCode>bulletType</InlineCode> to either{" "}
+        <InlineCode>"bullet"</InlineCode>, <InlineCode>"check"</InlineCode>,
+        {" "}
+        <InlineCode>"arrow"</InlineCode>, <InlineCode>"triangle"</InlineCode> or
+        {" "}
+        <InlineCode>"none"</InlineCode>. Defaults to{" "}
+        <InlineCode>"bullet"</InlineCode>.
       </div>
-      <Examples title="Default">
-        <UL>
+    ),
+    example: () => (
+      <Stack class="gap-5">
+        <UL bulletType="bullet">
           <li>One</li>
           <li>Two</li>
           <li>Three</li>
         </UL>
-      </Examples>
-      <Examples title="Outside">
-        <Stack class="gap-5">
-          <UL bulletLocation="outside" bulletType="bullet">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletLocation="outside" bulletType="check">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletLocation="outside" bulletType="arrow">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletLocation="outside" bulletType="triangle">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-        </Stack>
-      </Examples>
-      <Examples title="Bullet type">
-        <Stack class="gap-5">
-          <UL bulletType="bullet">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="check">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="arrow">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="triangle">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-        </Stack>
-      </Examples>
-      <Examples title="Bigger">
-        <Stack class="gap-5">
-          <UL bulletType="bullet" class="text-xl">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="check" class="text-xl">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="arrow" class="text-xl">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="triangle" class="text-xl">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-        </Stack>
-      </Examples>
-      <Examples title="Color">
-        <Stack class="gap-5">
-          <UL bulletType="bullet" class="text-neutral-500">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="check" class="text-neutral-500">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="arrow" class="text-neutral-500">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-          <UL bulletType="triangle" class="text-neutral-500">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </UL>
-        </Stack>
-      </Examples>
-      <Examples title="Custom">
-        <UL bulletType="none">
-          <li>
-            <Flex class="gap-2">
-              <span>😎</span>
-              One
-            </Flex>
-          </li>
-          <li>
-            <Flex class="gap-2">
-              <span>😎</span>
-              Two
-            </Flex>
-          </li>
-          <li>
-            <Flex class="gap-2">
-              <span>😎</span>
-              Three
-            </Flex>
-          </li>
+        <UL bulletType="check">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
         </UL>
-      </Examples>
-    </Stack>
+        <UL bulletType="arrow">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="triangle">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="none">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <UL bulletType="bullet">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="check">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="arrow">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="triangle">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="none">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Outside",
+    example: () => (
+      <Stack class="gap-5">
+        <UL bulletLocation="outside" bulletType="bullet">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletLocation="outside" bulletType="check">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletLocation="outside" bulletType="arrow">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletLocation="outside" bulletType="triangle">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <UL bulletLocation="outside" bulletType="bullet">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletLocation="outside" bulletType="check">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletLocation="outside" bulletType="arrow">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletLocation="outside" bulletType="triangle">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Bigger",
+    example: () => (
+      <Stack class="gap-5">
+        <UL bulletType="bullet" class="text-xl">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="check" class="text-xl">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="arrow" class="text-xl">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="triangle" class="text-xl">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <UL bulletType="bullet" class="text-xl">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="check" class="text-xl">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="arrow" class="text-xl">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="triangle" class="text-xl">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Color",
+    example: () => (
+      <Stack class="gap-5">
+        <UL bulletType="bullet" class="text-neutral-500">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="check" class="text-neutral-500">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="arrow" class="text-neutral-500">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+        <UL bulletType="triangle" class="text-neutral-500">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </UL>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <UL bulletType="bullet" class="text-neutral-500">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="check" class="text-neutral-500">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="arrow" class="text-neutral-500">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+  <UL bulletType="triangle" class="text-neutral-500">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </UL>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Custom",
+    example: () => (
+      <UL bulletType="none">
+        <li>
+          <Flex class="gap-2">
+            <span>😎</span>
+            One
+          </Flex>
+        </li>
+        <li>
+          <Flex class="gap-2">
+            <span>😎</span>
+            Two
+          </Flex>
+        </li>
+        <li>
+          <Flex class="gap-2">
+            <span>😎</span>
+            Three
+          </Flex>
+        </li>
+      </UL>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<UL bulletType="none">
+  <li>
+    <Flex class="gap-2">
+      <span>😎</span>
+      One
+    </Flex>
+  </li>
+  <li>
+    <Flex class="gap-2">
+      <span>😎</span>
+      Two
+    </Flex>
+  </li>
+  <li>
+    <Flex class="gap-2">
+      <span>😎</span>
+      Three
+    </Flex>
+  </li>
+</UL>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "bulletLocation",
+    required: false,
+    type: "'inside' | 'outside'",
+    default: "'inside'",
+    notes: null,
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="UL"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/ul.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };
+
+// export default () => {
+//   return (
+//     <Stack class="gap-8">
+//       <Examples title="Custom">
+//         <UL bulletType="none">
+//           <li>
+//             <Flex class="gap-2">
+//               <span>😎</span>
+//               One
+//             </Flex>
+//           </li>
+//           <li>
+//             <Flex class="gap-2">
+//               <span>😎</span>
+//               Two
+//             </Flex>
+//           </li>
+//           <li>
+//             <Flex class="gap-2">
+//               <span>😎</span>
+//               Three
+//             </Flex>
+//           </li>
+//         </UL>
+//       </Examples>
+//     </Stack>
+//   );
+// };

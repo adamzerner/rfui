622e503754587c994efd74266ad0fcd75433ece1
Adam Zerner <adamzerner@protonmail.com>
2023-12-21 23:24:27 -0800

    Add docs for `H2`

--- routes/atoms/h2.tsx
+++ routes/atoms/h2.tsx
@@ -1,21 +1,72 @@
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
 import { H2 } from "@/components/atoms/h2.tsx";
-import { Link } from "@/components/atoms/link.tsx";
-import { Stack } from "@/components/helpers/stack.tsx";
+import { Text } from "@/components/atoms/text.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
+  const sections = [{
+    title: "Basic",
+    example: () => <H2>Hello world</H2>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<H2>Hello world</H2>`}
+      />
+    ),
+  }, {
+    title: "Alongside paragraphs",
+    example: () => (
+      <div>
+        <H2>Hello world</H2>
+        <Text>
+          <p>
+            Illo quia quam perferendis ut consectetur rerum dolores dolores.
+            Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+            consectetur non amet nostrum quo animi.
+          </p>
+          <p>
+            Quod enim vitae et. Impedit ut maxime fugit excepturi harum
+            qui.Praesentium pariatur nisi ut rerum et dolores sed.
+          </p>
+        </Text>
+      </div>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<div>
+  <H2>Hello world</H2>
+  <Text>
+    <p>
+      Illo quia quam perferendis ut consectetur rerum dolores dolores.
+      Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+      consectetur non amet nostrum quo animi.
+    </p>
+    <p>
+      Quod enim vitae et. Impedit ut maxime fugit excepturi harum
+      qui.Praesentium pariatur nisi ut rerum et dolores sed.
+    </p>
+  </Text>
+</div>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
   return (
-    <Stack class="gap-8">
-      <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/h2.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/h2.tsx
-        </Link>
-      </div>
-      <H2>Header</H2>
-      <div>
-        Quod enim vitae et. Impedit ut maxime fugit excepturi harum qui.
-        Praesentium pariatur nisi ut rerum et dolores sed.
-      </div>
-    </Stack>
+    <ComponentDocsPage
+      componentName="H2"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/h2.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

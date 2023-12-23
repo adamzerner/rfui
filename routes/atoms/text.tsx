52b3b2f436114010ca74416e602c24ad42596929
Adam Zerner <adamzerner@protonmail.com>
2023-12-22 22:33:57 -0800

    Add docs for `Text`

--- routes/atoms/text.tsx
+++ routes/atoms/text.tsx
@@ -1,59 +1,167 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
 import { Text } from "@/components/atoms/text.tsx";
-import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => (
+      <Text>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
+          odio facilisis mauris sit.
+        </p>
+        <p>
+          Amet consectetur adipiscing elit duis tristique. Ac turpis egestas
+          maecenas pharetra. Dui nunc mattis enim ut tellus. Egestas congue
+          quisque egestas diam in.
+        </p>
+      </Text>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Text>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat odio facilisis mauris sit.</p>
+  <p>Amet consectetur adipiscing elit duis tristique. Ac turpis egestas maecenas pharetra. Dui nunc mattis enim ut tellus. Egestas congue quisque egestas diam in.</p>
+</Text>`}
+      />
+    ),
+  }, {
+    title: "Small",
+    description: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/atoms/text.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/atoms/text.tsx
-        </Link>
+        Set <InlineCode>size</InlineCode> to <InlineCode>"sm"</InlineCode>.
       </div>
-      <Examples title="Small">
-        <Text size="sm">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
-            odio facilisis mauris sit.
-          </p>
-          <p>
-            Amet consectetur adipiscing elit duis tristique. Ac turpis egestas
-            maecenas pharetra. Dui nunc mattis enim ut tellus. Egestas congue
-            quisque egestas diam in.
-          </p>
-        </Text>
-      </Examples>
-      <Examples title="Medium">
-        <Text size="md">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
-            odio facilisis mauris sit.
-          </p>
-          <p>
-            Amet consectetur adipiscing elit duis tristique. Ac turpis egestas
-            maecenas pharetra. Dui nunc mattis enim ut tellus. Egestas congue
-            quisque egestas diam in.
-          </p>
-        </Text>
-      </Examples>
-      <Examples title="Large">
-        <Text size="lg">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
-            odio facilisis mauris sit.
-          </p>
-          <p>
-            Amet consectetur adipiscing elit duis tristique. Ac turpis egestas
-            maecenas pharetra. Dui nunc mattis enim ut tellus. Egestas congue
-            quisque egestas diam in.
-          </p>
-        </Text>
-      </Examples>
-    </Stack>
+    ),
+    example: () => (
+      <Text size="sm">
+        <p>
+          Illo quia quam perferendis ut consectetur rerum dolores dolores.
+          Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+          consectetur non amet nostrum quo animi.
+        </p>
+        <p>
+          Quod enim vitae et. Impedit ut maxime fugit excepturi harum qui.
+          Praesentium pariatur nisi ut rerum et dolores sed.
+        </p>
+      </Text>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Text size="sm">
+  <p>
+    Illo quia quam perferendis ut consectetur rerum dolores dolores.
+    Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+    consectetur non amet nostrum quo animi.
+  </p>
+  <p>
+    Quod enim vitae et. Impedit ut maxime fugit excepturi harum
+    qui. Praesentium pariatur nisi ut rerum et dolores sed.
+  </p>
+</Text>`}
+      />
+    ),
+  }, {
+    title: "Medium",
+    description: (
+      <div>
+        Set <InlineCode>size</InlineCode> to <InlineCode>"md"</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Text size="md">
+        <p>
+          Illo quia quam perferendis ut consectetur rerum dolores dolores.
+          Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+          consectetur non amet nostrum quo animi.
+        </p>
+        <p>
+          Quod enim vitae et. Impedit ut maxime fugit excepturi harum qui.
+          Praesentium pariatur nisi ut rerum et dolores sed.
+        </p>
+      </Text>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Text size="md">
+  <p>
+    Illo quia quam perferendis ut consectetur rerum dolores dolores.
+    Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+    consectetur non amet nostrum quo animi.
+  </p>
+  <p>
+    Quod enim vitae et. Impedit ut maxime fugit excepturi harum
+    qui. Praesentium pariatur nisi ut rerum et dolores sed.
+  </p>
+</Text>`}
+      />
+    ),
+  }, {
+    title: "Large",
+    description: (
+      <div>
+        Set <InlineCode>size</InlineCode> to <InlineCode>"lg"</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Text size="lg">
+        <p>
+          Illo quia quam perferendis ut consectetur rerum dolores dolores.
+          Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+          consectetur non amet nostrum quo animi.
+        </p>
+        <p>
+          Quod enim vitae et. Impedit ut maxime fugit excepturi harum qui.
+          Praesentium pariatur nisi ut rerum et dolores sed.
+        </p>
+      </Text>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Text size="lg">
+  <p>
+    Illo quia quam perferendis ut consectetur rerum dolores dolores.
+    Reiciendis est beatae magnam ut amet quis sequi. Eaque consequatur
+    consectetur non amet nostrum quo animi.
+  </p>
+  <p>
+    Quod enim vitae et. Impedit ut maxime fugit excepturi harum
+    qui. Praesentium pariatur nisi ut rerum et dolores sed.
+  </p>
+</Text>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "size",
+    required: false,
+    type: "'sm' | 'md' | 'lg'",
+    default: '"md"',
+    notes: null,
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="Text"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/text.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

b31200839a1e4957a5faa3443f2c49db684a16a7
Adam Zerner <adamzerner@protonmail.com>
2023-12-22 22:42:43 -0800

    Add docs for `Textarea`

--- routes/atoms/textarea.tsx
+++ routes/atoms/textarea.tsx
@@ -1,33 +1,125 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
 import { Textarea } from "@/components/atoms/textarea.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => <Textarea></Textarea>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Textarea></Textarea>`}
+      />
+    ),
+  }, {
+    title: "Rounded",
+    description: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/helpers/textarea.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/helpers/textarea.tsx
-        </Link>
+        Set <InlineCode>rounded</InlineCode> to{" "}
+        <InlineCode>"square"</InlineCode>, <InlineCode>"sm"</InlineCode> or{" "}
+        <InlineCode>"lg"</InlineCode>.
       </div>
-      <Examples title="Rounded">
-        <Stack class="gap-5">
-          <Textarea rounded="square"></Textarea>
-          <Textarea rounded="sm"></Textarea>
-          <Textarea rounded="lg"></Textarea>
-        </Stack>
-      </Examples>
-      <Examples title="Disabled">
-        <Textarea disabled>example</Textarea>
-      </Examples>
-      <Examples title="Readonly">
-        <Textarea readonly>example</Textarea>
-      </Examples>
-      <Examples title="Invalid">
-        <Textarea invalid>example</Textarea>
-      </Examples>
-    </Stack>
+    ),
+    example: () => (
+      <Stack class="gap-5">
+        <Textarea rounded="square"></Textarea>
+        <Textarea rounded="sm"></Textarea>
+        <Textarea rounded="lg"></Textarea>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5">
+  <Textarea rounded="square"></Textarea>
+  <Textarea rounded="sm"></Textarea>
+  <Textarea rounded="lg"></Textarea>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Disabled",
+    description: (
+      <div>
+        Set <InlineCode>disabled</InlineCode> to <InlineCode>true</InlineCode>
+        {" "}
+        or <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => <Textarea disabled></Textarea>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Textarea disabled></Textarea>`}
+      />
+    ),
+  }, {
+    title: "Readonly",
+    description: (
+      <div>
+        Set <InlineCode>readonly</InlineCode> to <InlineCode>true</InlineCode>
+        {" "}
+        or <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => <Textarea readonly>Example</Textarea>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Textarea readonly>Example</Textarea>`}
+      />
+    ),
+  }, {
+    title: "Invalid",
+    description: (
+      <div>
+        Set <InlineCode>invalid</InlineCode> to <InlineCode>true</InlineCode> or
+        {" "}
+        <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => <Textarea invalid>Example</Textarea>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Textarea invalid>Example</Textarea>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "rounded",
+    required: false,
+    type: "'square' | 'sm' | 'lg'",
+    default: '"sm"',
+    notes: null,
+  }, {
+    name: "invalid",
+    required: false,
+    type: "boolean",
+    default: "false",
+    notes: null,
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="Textarea"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/textarea.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

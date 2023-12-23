49d7bbaf8a477193ff0ce4d9d08129d743ce6529
Adam Zerner <adamzerner@protonmail.com>
2023-12-23 00:08:13 -0800

    Add docs for `Alert`

--- routes/molecules/alert.tsx
+++ routes/molecules/alert.tsx
@@ -1,76 +1,189 @@
-import { Link } from "@/components/atoms/link.tsx";
+import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
-import { Examples } from "@/islands/demo/examples.tsx";
+import { ComponentDocsPage } from "@/islands/demo/component-docs-page.tsx";
 import { Alert, AlertBody, AlertHeader } from "@/islands/molecules/alert.tsx";
 
 export default () => {
-  return (
-    <Stack class="gap-8">
+  const sections = [{
+    title: "Basic",
+    example: () => <Alert>Your profile has been saved</Alert>,
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Alert>Your profile has been saved</Alert>,`}
+      />
+    ),
+  }, {
+    title: "AlertHeader and AlertBody",
+    example: () => (
+      <Alert>
+        <AlertHeader>Success</AlertHeader>
+        <AlertBody>Your profile has been saved.</AlertBody>
+      </Alert>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Alert>
+  <AlertHeader>Success</AlertHeader>
+  <AlertBody>Your profile has been saved.</AlertBody>
+</Alert>`}
+      />
+    ),
+  }, {
+    title: "Variant",
+    description: (
       <div>
-        <div class="text-2xl mb-3">Source code</div>
-        <Link href="https://github.com/adamzerner/rfui/blob/master/routes/molecules/alert.tsx">
-          https://github.com/adamzerner/rfui/blob/master/routes/molecules/alert.tsx
-        </Link>
+        Set <InlineCode>variant</InlineCode> to{" "}
+        <InlineCode>"success"</InlineCode>, <InlineCode>"info"</InlineCode>,
+        {" "}
+        <InlineCode>"warning"</InlineCode>,{" "}
+        <InlineCode>"danger"</InlineCode>, or{" "}
+        <InlineCode>"neutral"</InlineCode>.
       </div>
-      <Examples title="Default">
-        <Alert>
-          Example
-        </Alert>
-      </Examples>
-      <Examples title="AlertHeader and AlertBody">
-        <Alert>
+    ),
+    example: () => (
+      <Stack class="gap-5 w-full">
+        <Alert variant="success">
           <AlertHeader>Success</AlertHeader>
           <AlertBody>Your profile has been saved.</AlertBody>
         </Alert>
-      </Examples>
-      <Examples title="Variant">
-        <Stack class="gap-5 w-full">
-          <Alert variant="success">
-            <AlertHeader>Success</AlertHeader>
-            <AlertBody>Your profile has been saved.</AlertBody>
-          </Alert>
-          <Alert variant="info">
-            <AlertHeader>Note</AlertHeader>
-            <AlertBody>Your account has been updated.</AlertBody>
-          </Alert>
-          <Alert variant="warning">
-            <AlertHeader>Warning</AlertHeader>
-            <AlertBody>Your plan is set to expire.</AlertBody>
-          </Alert>
-          <Alert variant="danger">
-            <AlertHeader>Danger</AlertHeader>
-            <AlertBody>Your payment method is inactive.</AlertBody>
-          </Alert>
-          <Alert variant="neutral">
-            <AlertHeader>Note</AlertHeader>
-            <AlertBody>Your account has been updated.</AlertBody>
-          </Alert>
-        </Stack>
-      </Examples>
-      <Examples title="Not dismissable">
-        <Stack class="gap-5 w-full">
-          <Alert isDismissable={false} variant="success">
-            <AlertHeader>Success</AlertHeader>
-            <AlertBody>Your profile has been saved.</AlertBody>
-          </Alert>
-          <Alert isDismissable={false} variant="info">
-            <AlertHeader>Note</AlertHeader>
-            <AlertBody>Your account has been updated.</AlertBody>
-          </Alert>
-          <Alert isDismissable={false} variant="warning">
-            <AlertHeader>Warning</AlertHeader>
-            <AlertBody>Your plan is set to expire.</AlertBody>
-          </Alert>
-          <Alert isDismissable={false} variant="danger">
-            <AlertHeader>Danger</AlertHeader>
-            <AlertBody>Your payment method is inactive.</AlertBody>
-          </Alert>
-          <Alert isDismissable={false} variant="neutral">
-            <AlertHeader>Note</AlertHeader>
-            <AlertBody>Your account has been updated.</AlertBody>
-          </Alert>
-        </Stack>
-      </Examples>
-    </Stack>
+        <Alert variant="info">
+          <AlertHeader>Note</AlertHeader>
+          <AlertBody>Your account has been updated.</AlertBody>
+        </Alert>
+        <Alert variant="warning">
+          <AlertHeader>Warning</AlertHeader>
+          <AlertBody>Your plan is set to expire.</AlertBody>
+        </Alert>
+        <Alert variant="danger">
+          <AlertHeader>Danger</AlertHeader>
+          <AlertBody>Your payment method is inactive.</AlertBody>
+        </Alert>
+        <Alert variant="neutral">
+          <AlertHeader>Note</AlertHeader>
+          <AlertBody>Your account has been updated.</AlertBody>
+        </Alert>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5 w-full">
+  <Alert variant="success">
+    <AlertHeader>Success</AlertHeader>
+    <AlertBody>Your profile has been saved.</AlertBody>
+  </Alert>
+  <Alert variant="info">
+    <AlertHeader>Note</AlertHeader>
+    <AlertBody>Your account has been updated.</AlertBody>
+  </Alert>
+  <Alert variant="warning">
+    <AlertHeader>Warning</AlertHeader>
+    <AlertBody>Your plan is set to expire.</AlertBody>
+  </Alert>
+  <Alert variant="danger">
+    <AlertHeader>Danger</AlertHeader>
+    <AlertBody>Your payment method is inactive.</AlertBody>
+  </Alert>
+  <Alert variant="neutral">
+    <AlertHeader>Note</AlertHeader>
+    <AlertBody>Your account has been updated.</AlertBody>
+  </Alert>
+</Stack>`}
+      />
+    ),
+  }, {
+    title: "Not dismissable",
+    description: (
+      <div>
+        Set <InlineCode>isDismissable</InlineCode> to{" "}
+        <InlineCode>false</InlineCode>.
+      </div>
+    ),
+    example: () => (
+      <Stack class="gap-5 w-full">
+        <Alert isDismissable={false} variant="success">
+          <AlertHeader>Success</AlertHeader>
+          <AlertBody>Your profile has been saved.</AlertBody>
+        </Alert>
+        <Alert isDismissable={false} variant="info">
+          <AlertHeader>Note</AlertHeader>
+          <AlertBody>Your account has been updated.</AlertBody>
+        </Alert>
+        <Alert isDismissable={false} variant="warning">
+          <AlertHeader>Warning</AlertHeader>
+          <AlertBody>Your plan is set to expire.</AlertBody>
+        </Alert>
+        <Alert isDismissable={false} variant="danger">
+          <AlertHeader>Danger</AlertHeader>
+          <AlertBody>Your payment method is inactive.</AlertBody>
+        </Alert>
+        <Alert isDismissable={false} variant="neutral">
+          <AlertHeader>Note</AlertHeader>
+          <AlertBody>Your account has been updated.</AlertBody>
+        </Alert>
+      </Stack>
+    ),
+    exampleCode: () => (
+      <CodeBlock
+        class="mt-4"
+        language="tsx"
+        code={`<Stack class="gap-5 w-full">
+  <Alert isDismissable={false} variant="success">
+    <AlertHeader>Success</AlertHeader>
+    <AlertBody>Your profile has been saved.</AlertBody>
+  </Alert>
+  <Alert isDismissable={false} variant="info">
+    <AlertHeader>Note</AlertHeader>
+    <AlertBody>Your account has been updated.</AlertBody>
+  </Alert>
+  <Alert isDismissable={false} variant="warning">
+    <AlertHeader>Warning</AlertHeader>
+    <AlertBody>Your plan is set to expire.</AlertBody>
+  </Alert>
+  <Alert isDismissable={false} variant="danger">
+    <AlertHeader>Danger</AlertHeader>
+    <AlertBody>Your payment method is inactive.</AlertBody>
+  </Alert>
+  <Alert isDismissable={false} variant="neutral">
+    <AlertHeader>Note</AlertHeader>
+    <AlertBody>Your account has been updated.</AlertBody>
+  </Alert>
+</Stack>`}
+      />
+    ),
+  }];
+  const props = [{
+    name: "variant",
+    required: false,
+    type: "'success' | 'info' | 'warning' | 'danger' | 'neutral'",
+    default: '"neutral"',
+    notes: null,
+  }, {
+    name: "isDismissable",
+    required: false,
+    type: "boolean",
+    default: "true",
+    notes: null,
+  }, {
+    name: "children",
+    required: true,
+    type: "ComponentChild",
+    default: null,
+    notes: null,
+  }];
+
+  return (
+    <ComponentDocsPage
+      componentName="Alert"
+      sourceCodeUrl="https://github.com/adamzerner/rfui/blob/master/components/atoms/alert.tsx"
+      sections={sections}
+      props={props}
+    />
   );
 };

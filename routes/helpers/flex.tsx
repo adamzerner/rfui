51f935e45f03c89519299b34deb35fff993d85fe
Adam Zerner <adamzerner@protonmail.com>
2023-12-24 09:40:11 -0800

    Remove code comment

--- routes/helpers/flex.tsx
+++ routes/helpers/flex.tsx
@@ -41,27 +41,3 @@ export default () => {
     />
   );
 };
-
-// import { Link } from "@/components/atoms/link.tsx";
-// import { Flex } from "@/components/helpers/flex.tsx";
-// import { Stack } from "@/components/helpers/stack.tsx";
-
-// export default () => {
-//   return (
-//     <Stack class="gap-8">
-//       <div>
-//         <div class="text-2xl mb-3">Source code</div>
-//         <Link href="https://github.com/adamzerner/rfui/blob/master/routes/helpers/flex.tsx">
-//           https://github.com/adamzerner/rfui/blob/master/routes/helpers/flex.tsx
-//         </Link>
-//       </div>
-//       <div>
-//         <Flex class="gap-8">
-//           <div>left</div>
-//           <div>middle</div>
-//           <div>right</div>
-//         </Flex>
-//       </div>
-//     </Stack>
-//   );
-// };

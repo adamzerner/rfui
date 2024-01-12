bcb497f8a32c8fc2e8a77173d2bad839c6b55969
Adam Zerner <adamzerner@protonmail.com>
2024-01-11 18:12:50 -0800

    WIP switching between tabs

--- islands/molecules/tabs.tsx
+++ islands/molecules/tabs.tsx
@@ -1,3 +1,4 @@
+import { Signal, useSignal } from "@preact/signals";
 import type { ComponentChild } from "preact";
 import { Flex } from "../helpers/flex.tsx";
 
@@ -26,11 +27,13 @@ TODO:
 export const Tabs = (
   { tabNames, children }: TabsType,
 ) => {
+  const activeTab = useSignal<string>(tabNames[0]);
+
   return (
     <div>
       <Flex>
         {tabNames.map((tabName) => (
-          <Tab tabName={tabName} isActive={tabName === "First"} />
+          <Tab tabName={tabName} activeTab={activeTab} />
         ))}
       </Flex>
       <div class="mt-6">{children}</div>
@@ -38,15 +41,23 @@ export const Tabs = (
   );
 };
 
-const Tab = ({ tabName, isActive }: { tabName: string; isActive: boolean }) => {
+const Tab = (
+  { tabName, activeTab }: { tabName: string; activeTab: Signal<string> },
+) => {
+  const handleClick = () => {
+    activeTab.value = tabName;
+  };
+  const isActive = tabName === activeTab.value;
   let containerClass =
-    "border-b w-full text-center pb-4 cursor-pointer border-neutral-500";
+    "border-b w-full text-center py-4 cursor-pointer border-neutral-500";
 
   if (isActive) {
-    containerClass += " border-b-2";
+    containerClass += " border-b-2 text-neutral-900";
+  } else {
+    containerClass += " text-neutral-500";
   }
 
-  return <div class={containerClass}>{tabName}</div>;
+  return <div class={containerClass} onClick={handleClick}>{tabName}</div>;
 };
 
 export const TabSection = () => {

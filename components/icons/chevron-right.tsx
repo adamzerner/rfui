c0bf83650c14d49e848e092a46a2efeedc5b163f
Adam Zerner <adamzerner@protonmail.com>
2024-01-26 15:12:06 -0800

    Add `ChevronRightIcon`

---
+++ components/icons/chevron-right.tsx
@@ -0,0 +1,21 @@
+export const ChevronRightIcon = ({ ...rest }) => {
+  const { class: restClass, ...restWithoutClass } = rest;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`w-6 h-6 ${restClass}`}
+      {...restWithoutClass}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+};

198a96020aced93c522d4a120f4bc19e2ad84df6
Adam Zerner <adamzerner@protonmail.com>
2024-01-27 23:05:52 -0800

    Fix issues with background of none

--- components/molecules/navbar.tsx
+++ components/molecules/navbar.tsx
@@ -138,11 +138,11 @@ export const NavbarItem = (
 ) => {
   const { class: restClass, ...restWithoutClass } = rest;
   let containerClass =
-    "inline-block border-b border-b-neutral-200 sm:border-b-neutral-50 hover:bg-neutral-100/50 sm:hover:bg-neutral-50";
+    "inline-block border-b border-b-neutral-200 sm:border-b-neutral-50 max-sm:hover:bg-neutral-100/50";
 
   if (isActive) {
     containerClass +=
-      " font-bold pointer-events-none sm:font-normal sm:border-b-primary-500 bg-neutral-100 sm:bg-neutral-50";
+      " font-bold pointer-events-none sm:font-normal sm:border-b-primary-500 max-sm:bg-neutral-100";
   } else {
     containerClass += " sm:hover:border-b-neutral-500 text-neutral-700";
   }

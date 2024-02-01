f152ce4a4ba49830617e8efeeba86a4e41f5e9c5
Adam Zerner <adamzerner@protonmail.com>
2024-01-31 16:05:44 -0800

    Remove console logs

--- islands/molecules/tabs.tsx
+++ islands/molecules/tabs.tsx
@@ -24,7 +24,6 @@ export type TabsType = {
 export const Tabs = (
   { fullWidth = false, children, ...rest }: TabsType,
 ) => {
-  console.log("Debugging `Tabs`...");
   const tabNames = getTabNames(children);
   const [activeTab, setActiveTab] = useState<string>(tabNames[0]);
   const activeTabSection = getActiveTabSection(children, activeTab);
@@ -49,7 +48,6 @@ export const Tabs = (
 };
 
 const getTabNames = (_children: any) => {
-  console.log(_children);
   const children = getChildren(_children);
 
   if (!Array.isArray(children)) {

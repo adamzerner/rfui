41397591eaa982a2483b637269e1cdb66e728a17
Adam Zerner <adamzerner@protonmail.com>
2024-02-09 22:33:12 -0800

    Update _404.tsx

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,7 +6,7 @@ export default () => {
     <div class="max-w-prose text-lg">
       <H1>Page not found</H1>
       <Text>
-        <p>The page you tried to access does not exist on rfui.com.</p>
+        <p>The page you tried to access does not exist on rfui.deno.dev.</p>
       </Text>
     </div>
   );

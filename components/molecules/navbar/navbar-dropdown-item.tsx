1928318e52f498424dfa4b1b42447c897c89e595
Adam Zerner <adamzerner@protonmail.com>
2024-12-22 18:29:48 -0800

    Improve UI of mobile dropdown

--- components/molecules/navbar/navbar-dropdown-item.tsx
+++ components/molecules/navbar/navbar-dropdown-item.tsx
@@ -1,5 +1,7 @@
 import type { ComponentChild } from "preact";
 import { Link } from "../../atoms/link.tsx";
+import { Container } from "../../helpers/container.tsx";
+import { ChevronRightIcon } from "../../icons/chevron-right.tsx";
 
 export const NavbarDropdownItem = (
   { href, children }: { href: string; children: ComponentChild },
@@ -7,13 +9,17 @@ export const NavbarDropdownItem = (
   return (
     <>
       {/* Mobile */}
-      <li class="block sm:hidden">
-        <Link
-          href={href}
-          class="block break-all py-4"
-        >
-          {children}
-        </Link>
+      <li class="block border-b border-b-neutral-200 text-neutral-700 max-sm:hover:bg-neutral-100/50 sm:hidden sm:border-b-neutral-50 sm:hover:border-b-neutral-500">
+        <Container size="xl" class="block sm:hidden">
+          <Link
+            href={href}
+            underline="none"
+            class="flex items-center justify-between gap-4 py-6"
+          >
+            <div class="grow break-all">{children}</div>
+            <ChevronRightIcon class="relative top-px min-w-fit" />
+          </Link>
+        </Container>
       </li>
 
       {/* Desktop */}
--- islands/molecules/navbar-dropdown.tsx
+++ islands/molecules/navbar-dropdown.tsx
@@ -37,43 +37,50 @@ export const NavbarDropdown = (
   }, []);
 
   return (
-    <li
-      class={containerClass}
-      ref={menuRef}
-      onClick={toggleMenu}
-      {...restWithoutClass}
-    >
-      {/* Desktop */}
-      <div class="hidden sm:block">
-        <span class="mr-1">{title}</span> {isMenuOpen
-          ? (
-            <ChevronUpIcon
-              className="relative bottom-[2px]"
-              strokeWidth={2}
-            />
-          )
-          : <ChevronDownIcon strokeWidth={2} />}
-      </div>
-      {isMenuOpen && (
-        <ul class="absolute left-0 top-[89px] hidden w-72 rounded border border-neutral-100 py-2 sm:block">
-          {children}
-        </ul>
-      )}
-
-      {/* Mobile */}
-      <Container size="xl" class="block sm:hidden">
-        <Flex class="block items-center justify-between sm:hidden">
-          <span>{title}</span>
-          {isMenuOpen
-            ? <ChevronUpIcon className="mr-[2px] h-6 w-6" strokeWidth={1} />
-            : <ChevronDownIcon className="mr-[2px] h-6 w-6" strokeWidth={1} />}
-        </Flex>
+    <>
+      <li
+        class={containerClass}
+        ref={menuRef}
+        onClick={toggleMenu}
+        {...restWithoutClass}
+      >
+        {/* Desktop */}
+        <div class="hidden sm:block">
+          <span class="mr-1">{title}</span> {isMenuOpen
+            ? (
+              <ChevronUpIcon
+                className="relative bottom-[2px]"
+                strokeWidth={2}
+              />
+            )
+            : <ChevronDownIcon strokeWidth={2} />}
+        </div>
         {isMenuOpen && (
-          <ul class="my-6 rounded border border-neutral-200/75 p-6">
+          <ul class="absolute left-0 top-[89px] hidden w-72 rounded border border-neutral-100 py-2 sm:block">
             {children}
           </ul>
         )}
-      </Container>
-    </li>
+
+        {/* Mobile */}
+        <Container size="xl" class="block sm:hidden">
+          <Flex class="block items-center justify-between sm:hidden">
+            <span>{title}</span>
+            {isMenuOpen
+              ? <ChevronUpIcon className="mr-[2px] h-6 w-6" strokeWidth={1} />
+              : (
+                <ChevronDownIcon
+                  className="mr-[2px] h-6 w-6"
+                  strokeWidth={1}
+                />
+              )}
+          </Flex>
+        </Container>
+      </li>
+      {isMenuOpen && (
+        <li class="block sm:hidden">
+          <ul class="ml-8">{children}</ul>
+        </li>
+      )}
+    </>
   );
 };

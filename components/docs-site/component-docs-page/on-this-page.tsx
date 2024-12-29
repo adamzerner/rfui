211ba31573a8127abfb19711cb4e2e2899f360bc
Adam Zerner <adamzerner@protonmail.com>
2024-12-29 14:45:18 -0800

    Make "On this page" sticky

--- components/docs-site/component-docs-page/on-this-page.tsx
+++ components/docs-site/component-docs-page/on-this-page.tsx
@@ -3,7 +3,7 @@ import { Stack } from "@/components/helpers/stack.tsx";
 
 export const OnThisPage = ({ sectionTitles }: { sectionTitles: string[] }) => {
   return (
-    <nav>
+    <nav class="sticky top-8">
       <div class="mb-4 font-bold text-neutral-700">On this page</div>
       <Stack class="gap-2">
         {sectionTitles.map((sectionTitle) => (

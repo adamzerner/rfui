a445821325da39a5d6e480aaaa42e945ae833395
Adam Zerner <adamzerner@protonmail.com>
2024-12-29 19:52:23 -0800

    Use `getSectionTitleHref`

--- components/docs-site/component-docs-page/on-this-page.tsx
+++ components/docs-site/component-docs-page/on-this-page.tsx
@@ -2,6 +2,9 @@ import { Link } from "@/components/atoms/link.tsx";
 import { Stack } from "@/components/helpers/stack.tsx";
 
 export const OnThisPage = ({ sectionTitles }: { sectionTitles: string[] }) => {
+  const getSectionTitleHref = (sectionTitle: string) =>
+    `#${sectionTitle.toLowerCase().split(/\s+/).join("-")}`;
+
   return (
     <nav class="sticky top-8">
       <div class="mb-4 font-bold text-neutral-700">On this page</div>
@@ -10,7 +13,7 @@ export const OnThisPage = ({ sectionTitles }: { sectionTitles: string[] }) => {
           <Link
             inPageLink
             underline="hover"
-            href={`#${sectionTitle.toLowerCase().split(/\s+/).join("-")}`}
+            href={getSectionTitleHref(sectionTitle)}
           >
             {sectionTitle}
           </Link>

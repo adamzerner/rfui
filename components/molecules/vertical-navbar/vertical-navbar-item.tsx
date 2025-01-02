c4f69cc32121467c35cb9488666fc7522b8a687e
Adam Zerner <adamzerner@protonmail.com>
2025-01-01 17:50:58 -0800

    Icons

--- components/molecules/vertical-navbar/vertical-navbar-item.tsx
+++ components/molecules/vertical-navbar/vertical-navbar-item.tsx
@@ -7,30 +7,42 @@ export const VerticalNavbarItem = (
     isActive,
     shouldOpenInNewTab = false,
     shouldIncludeNewTabIcon = false,
+    icon,
     children,
   }: {
     href: string;
     isActive: boolean;
     shouldOpenInNewTab?: boolean;
     shouldIncludeNewTabIcon?: boolean;
+    icon?: ComponentChild;
     children: ComponentChild;
   },
 ) => {
-  let containerClass = "my-2 block rounded px-3 py-2";
+  let containerClass = "my-2 block flex items-center rounded px-3 py-2";
 
   containerClass += ` ${isActive ? "font-bold" : "hover:bg-neutral-100"}`;
 
+  const iconClass = "mr-3 h-[20px] w-[20px] opacity-50";
+
   return (
-    isActive ? <div class={containerClass}>{children}</div> : (
-      <Link
-        href={href}
-        class={containerClass}
-        underline="none"
-        _newTab={shouldOpenInNewTab}
-        _includeNewTabIcon={shouldIncludeNewTabIcon}
-      >
-        {children}
-      </Link>
-    )
+    isActive
+      ? (
+        <div class={containerClass}>
+          {icon && <span class={iconClass}>{icon}</span>}
+          {children}
+        </div>
+      )
+      : (
+        <Link
+          href={href}
+          class={containerClass}
+          underline="none"
+          _newTab={shouldOpenInNewTab}
+          _includeNewTabIcon={shouldIncludeNewTabIcon}
+        >
+          {icon && <span class={iconClass}>{icon}</span>}
+          {children}
+        </Link>
+      )
   );
 };

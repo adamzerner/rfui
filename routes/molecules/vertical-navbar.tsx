076691178c0b685d416d663c0aaa4c180efb9f22
Adam Zerner <adamzerner@protonmail.com>
2025-01-01 22:11:18 -0800

    Add docs for props tables for `VerticalNavbar`

--- routes/molecules/vertical-navbar.tsx
+++ routes/molecules/vertical-navbar.tsx
@@ -1,4 +1,6 @@
 import { CodeBlock } from "@/components/atoms/code-block.tsx";
+import { InlineCode } from "@/components/atoms/inline-code.tsx";
+import { Link } from "@/components/atoms/link.tsx";
 import {
   ComponentDocsPage,
   ExampleType,
@@ -42,6 +44,118 @@ export default () => {
       type: '"neutral" | "none"',
       default: '"neutral"',
       notes: null,
+    }, {
+      name: "children",
+      required: true,
+      type: "ComponentChild",
+      default: null,
+      notes: null,
+    }, {
+      name: "...rest",
+      required: false,
+      type: 'Omit<ComponentProps<"nav">, "size">',
+      default: null,
+      notes: (
+        <div>
+          <div class="leading-relaxed">
+            See the docs for{" "}
+            <Link href="/rest-parameters">rest parameters</Link>. For{" "}
+            <InlineCode>VerticalNavbar</InlineCode>, you could pass anything you
+            normally would pass to <InlineCode>{"<nav>"}</InlineCode>{" "}
+            because the return value{" "}
+            <Link href="https://github.com/adamzerner/rfui/blob/master/islands/molecules/vertical-navbar.tsx">
+              looks something like
+            </Link>{" "}
+            this:
+          </div>
+          <CodeBlock
+            language="tsx"
+            code={`<nav class={containerClass} {...restWithoutClass}>
+  ...
+</nav>`}
+          />
+        </div>
+      ),
+    }],
+  }, {
+    title: "VerticalNavbarTop",
+    props: [{
+      name: "children",
+      required: true,
+      type: "ComponentChild",
+      default: null,
+      notes: null,
+    }],
+  }, {
+    title: "VerticalNavbarBottom",
+    props: [{
+      name: "children",
+      required: true,
+      type: "ComponentChild",
+      default: null,
+      notes: null,
+    }],
+  }, {
+    title: "VerticalNavbarSection",
+    props: [{
+      name: "heading",
+      required: false,
+      type: "string",
+      default: null,
+      notes: null,
+    }, {
+      name: "children",
+      required: true,
+      type: "ComponentChild",
+      default: null,
+      notes: null,
+    }],
+  }, {
+    title: "VerticalNavbarItem",
+    props: [{
+      name: "href",
+      required: true,
+      type: "string",
+      default: null,
+      notes: null,
+    }, {
+      name: "isActive",
+      required: true,
+      type: "boolean",
+      default: null,
+      notes: null,
+    }, {
+      name: "shouldOpenInNewTab",
+      required: false,
+      type: "boolean",
+      default: "false",
+      notes: (
+        <div>
+          See the <Link href="/atoms/link#example-new-tab">docs</Link>{" "}
+          for the corresponding property in the <InlineCode>Link</InlineCode>
+          {" "}
+          component.
+        </div>
+      ),
+    }, {
+      name: "shouldIncludeNewTabIcon",
+      required: false,
+      type: "boolean",
+      default: "false",
+      notes: (
+        <div>
+          See the <Link href="/atoms/link#example-new-tab">docs</Link>{" "}
+          for the corresponding property in the <InlineCode>Link</InlineCode>
+          {" "}
+          component.
+        </div>
+      ),
+    }, {
+      name: "icon",
+      required: false,
+      type: "ComponentChild",
+      default: null,
+      notes: "A 20x20 icon to be placed to the left of the text.",
     }, {
       name: "children",
       required: true,

214847879047b0d5ff406fe9414e0098c95e7105
Adam Zerner <adamzerner@protonmail.com>
2025-01-01 16:54:02 -0800

    Get rid of footer

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,20 +1,16 @@
 import { PageProps } from "$fresh/server.ts";
-import { Footer } from "@/components/docs-site/footer.tsx";
 import { VerticalNavbar } from "@/components/docs-site/vertical-navbar.tsx";
 import { Container } from "@/components/helpers/container.tsx";
 import { Flex } from "@/components/helpers/flex.tsx";
 
 export default ({ Component, route }: PageProps) => {
   return (
-    <>
-      <Flex>
-        <VerticalNavbar route={route} />
-        <Container size="2xl" class="grow font-sans">
-          <Component />
-        </Container>
-      </Flex>
-      <Footer />
-    </>
+    <Flex>
+      <VerticalNavbar route={route} />
+      <Container size="2xl" class="grow pb-8 font-sans">
+        <Component />
+      </Container>
+    </Flex>
   );
 
   // return (

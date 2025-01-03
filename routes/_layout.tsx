9f8a510b07bcb80740cefcf6f9a38d6c0f551b94
Adam Zerner <adamzerner@protonmail.com>
2025-01-02 16:26:25 -0800

    Vertical navbar docs (#243)
    * Add overview notes
    
    * FIx weird issue related to exports
    
    Exporting `VerticalNavbarItem` was causing the icons to not work for some reason.
    
    * Add basic example
    
    * Add two notes
    
    * Tweak
    
    * Add remaining docs
    
    * Use original navbar

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,24 +1,17 @@
 import { PageProps } from "$fresh/server.ts";
-import { VerticalNavbar } from "@/components/docs-site/vertical-navbar.tsx";
+import { Footer } from "@/components/docs-site/footer.tsx";
+import { Navbar } from "@/components/docs-site/navbar.tsx";
 import { Container } from "@/components/helpers/container.tsx";
+import { Stack } from "@/components/helpers/stack.tsx";
 
 export default ({ Component, route }: PageProps) => {
   return (
-    <section class="sm:flex">
-      <VerticalNavbar route={route} />
-      <Container size="2xl" class="grow pb-8 font-sans">
+    <Stack class="min-h-screen">
+      <Navbar route={route} />
+      <Container size="2xl" class="mb-6 mt-8 grow font-sans">
         <Component />
       </Container>
-    </section>
+      <Footer />
+    </Stack>
   );
-
-  // return (
-  //   <Stack class="min-h-screen">
-  //     <Navbar route={route} />
-  //     <Container size="2xl" class="mb-6 mt-8 grow font-sans">
-  //       <Component />
-  //     </Container>
-  //     <Footer />
-  //   </Stack>
-  // );
 };
